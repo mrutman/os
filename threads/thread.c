5454f1a962fc2bc3dd18b015ae8050622e28c4cd
Mikhail Rutman <m.rutman@postgrespro.ru>
2023-09-03 19:01:04 +0700

    add starting code for tasks: threads and sync

---
+++ threads/thread.c
@@ -0,0 +1,28 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void *mythread(void *arg) {
+	printf("mythread [%d %d %d]: Hello from mythread!\n", getpid(), getppid(), gettid());
+	return NULL;
+}
+
+int main() {
+	pthread_t tid;
+	int err;
+
+	printf("main [%d %d %d]: Hello from main!\n", getpid(), getppid(), gettid());
+
+	err = pthread_create(&tid, NULL, mythread, NULL);
+	if (err) {
+	    printf("main: pthread_create() failed: %s\n", strerror(err));
+		return -1;
+	}
+
+	return 0;
+}
+
